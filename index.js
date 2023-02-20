c196ddb0fe95992d3b243926fa089300c0753a02
Alex Kosla <alexkosla@hotmail.com>
2023-02-20 14:51:40 +0000

    added sidebar to all pages

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-function hideUpward() {
-    $(document).ready(function(){
-        $("#upward").click(function(){
-          $("#upward").hide();
-        });
-      });
-      console.log("hiding upward");
-  }
-
-function toggleHamburgerMenu(){
-    console.log("activating hamburger menu");
-    $('class-sidebar').animate({width: 'toggle'});
-}
-
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+/* Set the width of the sidebar to 220px and the left margin of the page content to 220px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "220px";
   document.getElementById("main").style.marginLeft = "220px";
-  // document.getElementById("indexmenu").className = "active";
-  // document.getElementById("indexmenu").onclick = "closeNav()";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
-  // document.getElementById("indexmenu").className = "";
-  // document.getElementById("indexmenu").onclick = "openNav()";
 } 
