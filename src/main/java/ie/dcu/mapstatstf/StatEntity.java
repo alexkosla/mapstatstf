b44adf10fd7b556b50446082e3eea820e27cfb6b
alexkosla <alexkosla@hotmail.com>
2023-04-03 15:12:07 +0100

    more cleanup for Q2

--- src/main/java/ie/dcu/mapstatstf/StatEntity.java
+++ src/main/java/ie/dcu/mapstatstf/StatEntity.java
@@ -1,21 +1,21 @@
-package ie.dcu.mapstatstf;
-
-import java.util.UUID;
-
-public class StatEntity {
-
-    private UUID statId;
-    private long logId;
-    private long steam64Id;
-
-    private int mapId;
-
-    private String className;
-    private int kills;
-    private int assists;
-    private int deaths;
-    private int damage;
-    private int damageTaken;
-    private int secondsPlayed;
-
-}
+//package ie.dcu.mapstatstf;
+//
+//import java.util.UUID;
+//
+//public class StatEntity {
+//
+//    private UUID statId;
+//    private long logId;
+//    private long steam64Id;
+//
+//    private int mapId;
+//
+//    private String className;
+//    private int kills;
+//    private int assists;
+//    private int deaths;
+//    private int damage;
+//    private int damageTaken;
+//    private int secondsPlayed;
+//
+//}
--- src/main/java/ie/dcu/mapstatstf/StatMapper.java
+++ src/main/java/ie/dcu/mapstatstf/StatMapper.java
@@ -1,14 +1,14 @@
-package ie.dcu.mapstatstf;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants;
-
-// automapper to map between DTO and Model, as needed
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
-public interface StatMapper {
-    @Mapping(source = "subject", target = "title")
-    StatModel entityToDto(StatEntity entity);
-
-    @Mapping(source = "title", target = "subject")
-    StatEntity toEntity(StatModel model);
-}
+//package ie.dcu.mapstatstf;
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//import org.mapstruct.MappingConstants;
+//
+//// automapper to map between DTO and Model, as needed
+//@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+//public interface StatMapper {
+//    @Mapping(source = "subject", target = "title")
+//    StatModel entityToDto(StatEntity entity);
+//
+//    @Mapping(source = "title", target = "subject")
+//    StatEntity toEntity(StatModel model);
+//}
