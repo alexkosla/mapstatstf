1656784c8e1c96dec1353f0e6f60076f7fc8a2ab
alexkosla <alexkosla@hotmail.com>
2024-09-19 17:49:57 +0100

    Fixed non-compilation issue by removing @Autowired above private StatRepository, because it's an interface, not a class.

--- src/main/java/ie/dcu/mapstatstf/Service/StatService.java
+++ src/main/java/ie/dcu/mapstatstf/Service/StatService.java
@@ -17,7 +17,6 @@ import java.util.stream.Collectors;
 
 @Service
 public class StatService {
-    @Autowired
     private StatRepository statRepository;
     // default constructor
     public StatService ()
